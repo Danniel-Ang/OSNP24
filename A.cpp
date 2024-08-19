f7b119495ea59ff153adade602a8359e63212137
Danniel-Ang <danniel@ui.ac.id>
2024-08-19 09:41:38 +0700

    Berhasil AC semua :3

--- A.cpp
+++ A.cpp
@@ -2,27 +2,34 @@
 #include <string>
 using namespace std;
 
-int cek(string s, string uji){
-    int ans = 0;
-    int i = 0;
-    for(char c : s){
-        if(c != uji[i]) continue;
-        ans++;
-        i++;
-        i %= 3;
+string s; 
+
+// Menghitung panjang string cantik dengan pola tertentu pada S
+int cek(string pola){ // OSN, ONS, 0 -> 1 -> 2 -> 0
+    int j = 0;
+    int ans = 0; // Panjang string cantik osn saat ini
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] == pola[j]){
+            ans++;
+            j++;
+            j %= 3;
+        }
     }
     return ans;
 }
 
 int main(){
-    string s; cin >> s;
-    int ans = 0;
-    ans = max(ans, cek(s, "OSN"));
-    ans = max(ans, cek(s, "ONS"));
-    ans = max(ans, cek(s, "SON"));
-    ans = max(ans, cek(s, "SNO"));
-    ans = max(ans, cek(s, "NOS"));
-    ans = max(ans, cek(s, "NSO"));
-    if(ans < 3) cout << -1;
-    else cout << ans;
+    cin >> s;
+    int maxx = 0;
+    maxx = max(maxx, cek("OSN"));
+    maxx = max(maxx, cek("ONS"));
+    maxx = max(maxx, cek("SON"));
+    maxx = max(maxx, cek("SNO"));
+    maxx = max(maxx, cek("NOS"));
+    maxx = max(maxx, cek("NSO"));
+    if(maxx < 3){
+        cout << -1;
+    }else{
+        cout << maxx;
+    }
 }
---
+++ D.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+// 16
+// 114240
+int main(){
+    string b, y;
+    cin >> b >> y;
+    long long dp[y.length()+1];
+    long long mod = 1000000007;
+
+    memset(dp, 0ll, sizeof(dp));
+    dp[y.length()] = 1;
+    for(int i = y.length()-1; i >= 0; i--){
+
+        string temp = "";
+        temp += y[i];
+        
+        if(stoi(temp) >= stoi(b)) break;
+        dp[i] = dp[i+1];
+        
+        if(y[i] == '0') continue;
+
+        for(int j = 1; j < b.length() && i + j < y.length(); j++){
+            temp += y[i+j];
+            if(stoi(temp) < stoi(b)){
+                dp[i] += dp[i+j+1];
+                dp[i] %= mod;
+            }else{
+                break;
+            }
+        }
+    }
+    cout << dp[0] << endl;
+}
+
