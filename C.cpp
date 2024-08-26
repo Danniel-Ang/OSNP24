711e750deab5ddc3125e95058ebe448181814fa0
Danniel-Ang <danniel@ui.ac.id>
2024-08-26 14:11:00 +0700

    Memperbarui Kode Problem C menjadi lebih simpel

--- C.cpp
+++ C.cpp
@@ -1,66 +1,38 @@
 #include <bits/stdc++.h>
 typedef long long ll;
-typedef unsigned long long ull;
-#define pb push_back
-#define eb emplace_back
-#define ppb pop_back
-#define fi first
-#define se second
-#define ub upper_bound
-#define lb lower_bound
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define rep(i,n) for (i = 0; i < n; ++i)
-#define REP(i,k,n) for (i = k; i <= n; ++i)
-#define REPR(i,k,n) for (i = k; i >= n; --i)
 using namespace std;
-const ll MOD = 1e9 + 7;
-#define semoga_ac ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
-typedef complex<double> P;
-#define X real()
-#define Y imag()
-typedef vector<int> vi;
-typedef vector<vi> vvi;
+ll ar[1000001];
 
-main(){
-semoga_ac;
-    ll n, i, temp; cin >> n;
-    ll m; cin >> m;
-    vector <ll> v;
-    rep(i, n){
-        cin >> temp;
-        v.pb(temp);
-    }
-    sort(v.begin(), v.end(), greater<ll>());
+int main(){
+     ll n, m;
+     cin >> n >> m;
+     
+     ll maxx = -1;
 
-    ll acc = 0, befa;
-    REP(i, 1, n-1){
-        befa = acc;
-        acc += (i) * (v[i-1] - v[i]);
-        if(acc >= m){
-            ll adder = i;
-            temp = m - befa;
-            // cout << adder << endl;
-            temp = (temp / adder) + min(1ll, temp % adder);
-            // cout << temp << " " << v[i-1] << " ";
-            cout << v[i-1] - temp;  
-            return 0;
-        }
-        // cout << acc << endl;
-    }
-    befa = acc;
-    acc += (v[n-1]) * n;
-    if(acc >= m){
-        ll adder = i;
-        temp = m - befa;
-        // cout << adder << endl;
-        temp = (temp / adder) + min(1ll, temp % adder);
-        // cout << temp << " " << v[i-1] << " ";
-        cout << v[i-1] - temp;  
-        return 0;
-    }
-    cout << -1;
-    // cout << acc << endl;
+     for(int i = 0; i < n; i++){
+          ll in; cin >> in;
+          ar[in-1]++;
+          maxx = max(maxx, in-1);
+     }   
 
+     ll cnt = ar[maxx];
+     for(int i = maxx-1; i >= 0; i--){
+          cnt += ar[i];
+          ar[i] = ar[i+1] + cnt;
+          // cout << i << " " << ar[i] << endl;
+     }
+
+     int l, r, mid; l = 0; r = maxx;
+     ll ans = -1;
+     while(l <= r){
+          mid = (l + r) >> 1;
+          if(ar[mid] >= m){
+               ans = mid;
+               l = mid + 1;
+          }else{
+               r = mid - 1;
+          }
+     }
+     cout << ans;
 }
