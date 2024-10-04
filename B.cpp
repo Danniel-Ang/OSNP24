6ec52700eb4c9479c92b069dc8d4a93b1ebcbc2e
Danniel-Ang <danniel@ui.ac.id>
2024-10-04 12:21:09 +0700

    Fixing B

--- B.cpp
+++ B.cpp
@@ -3,7 +3,21 @@ typedef long long ll;
 using namespace std;
 
 main(){
-    if(a == b){
-        // code blocks
-    }`
+    ll b, c, d; cin >> b >> c >> d;
+    ll n = c - d;
+    if(c == d){
+        cout << max(b - c, 0ll);
+    }else{
+        ll ans = 0;
+
+        // Iterasi 1 -> sqrt(n)
+        for(ll i = 1; i * i <= n; i++){
+            if(n % i != 0) continue;
+
+            ll fac2 = n / i;
+            if(i <= b && i > d) ans++;
+            if(fac2 <= b && fac2 > d && fac2 != i) ans++;
+        }
+        cout << ans;
+    }
 }   
--- E.cpp
+++ E.cpp
@@ -1,77 +1,39 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
+#include <bits/stdc++.h>
 typedef long long ll;
+typedef unsigned long long ull;
+#define pb push_back
+#define eb emplace_back
+#define ppb pop_back
+#define fi first
+#define se second
+#define ub upper_bound
+#define lb lower_bound
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define rep(i,n) for (i = 0; i < n; ++i)
+#define REP(i,k,n) for (i = k; i <= n; ++i)
+#define REPR(i,k,n) for (i = k; i >= n; --i)
 using namespace std;
+const ll MOD = 1e9 + 7;
+#define semoga_ac ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
-int n, m, k;
-ll sum = 0;
-ll b[100001];
-ll g[100001];
-vector <int> adj[100001];
-bool vis[100001];
-
-int main(){
-    cin >> n >> m >> k;
-    for(int i = 1; i <= n; i++){
-        cin >> b[i];
+typedef complex<double> P;
+#define X real()
+#define Y imag()
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+int exampleFunction(int n) {
+    if (n <= 1) {
+        return 1;
+    } else if (n % 2 == 0) {
+        return 1 + exampleFunction (n / 2) + exampleFunction (n / 2 - 1);
+    } else {
+        return 1 + exampleFunction (n - 1) + exampleFunction (n - 3);
     }
-    for(int i = 1; i <= m; i++){
-        cin >> g[i];
+}
+main(){
+semoga_ac;
+    for(int i = 1; i <= 100; i++){
+        cout << i << " " << exampleFunction(i) << endl;
     }
-    sort(g + 1, g + m + 1);
-
-    for(int i = 1; i <= k; i++){
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-
-    for(int i = 1; i <= n; i++){
-        if(vis[i]) continue;
-        vis[i] = true;
-
-        queue <int> q;
-        vector <ll> v;
-
-        v.push_back(b[i]);
-        q.push(i);
-
-        while(!q.empty()){
-            int now = q.front();
-            q.pop();
-
-            for(int u: adj[now]){
-                if(vis[u]) continue;
-                vis[u] = true;
-                q.push(u);
-                v.push_back(b[u]);
-            }
-        }
-        sort(v.begin(), v.end());
-        ll med = v[v.size() / 2];
-
-        int l, r, mid, save = 1;
-        l = 1; r = m;
-        while(l <= r){
-            mid = (l + r) / 2;
-            if(g[mid] <= med){
-                save = mid;
-                l = mid + 1;
-            } else r = mid - 1;
-        }
-
-        ll ans1 = 0;
-        ll ans2 = 0;
-        for(ll u: v){
-            ans1 += abs(u - g[save]);
-            if(save + 1 <= m) ans2 += abs(u - g[save + 1]);
-        }
-        if(save + 1 <= m) sum += min(ans1, ans2);
-        else sum += ans1;
-        // cout << sum << endl;
-    }
-    cout << sum;
 }
